45f4612541564678ebed178e3157bb3fd13858a2
Malachi <mpeel9938@gmail.com>
2024-10-03 16:52:41 -0700

    styling issues when changing between reg and log forms // Base styling complete // revisit

--- game/register_login.js
+++ game/register_login.js
@@ -3,9 +3,13 @@ const signup_BTN = document.getElementById('SignUpButton')
 const register_section = document.getElementById('register-form')
 const login_section = document.getElementById('login-form')
 
-signup_BTN.addEventListener(click, function(){
+signup_BTN.addEventListener('click', function(){
+    register_section.style.display="block";
+    login_section.style.display="none";
 
 })
-login_BTN_BTN.addEventListener(click, function(){
-    
+
+login_BTN.addEventListener('click', function(){
+    register_section.style.display="none";
+    login_section.style.display="block";
 })
